7965bd7c954eff9d509241eed48876dd515e9304
waleedemadyaya <waleedemad797@gmail.com>
2024-04-28 09:39:15 +0300

    22

--- src/devices/timer.h
+++ src/devices/timer.h
@@ -3,9 +3,23 @@
 
 #include <round.h>
 #include <stdint.h>
+#include "lib/kernel/list.h"/*----for usage of list abstract data types----*/
+#include "threads/synch.h"/*----for usage of semaphores for locking----*/
+#include "threads/thread.h"/*----for usage of threads functions----*/
 
 /* Number of timer interrupts per second. */
 #define TIMER_FREQ 100
+
+/*----struct for storing data about each element inside the sleeping list----*/
+struct sleepy_thread_elem
+{
+    struct list_elem list_elem_val ; //the element value inside the linked list
+    int64_t tick_to_walke_up ; //expected time for walke up in ticks
+    struct semaphore* thread_sema; //semaphore for synchronization
+    struct thread* current_thread; //data about thread 
+
+};
+/*----*******************************************************************----*/
 
 void timer_init (void);
 void timer_calibrate (void);
